68a42dc6a77a108c5a4d852fb7e0a51dd638d2f2
Sachin Ghugare <sachinghugare309@gmail.com>
2020-05-04 13:42:52 +0530

    changes 2 in homepage

--- app/src/main/java/com/example/smarteducation/homepage.java
+++ app/src/main/java/com/example/smarteducation/homepage.java
@@ -1,14 +1,33 @@
 package com.example.smarteducation;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class homepage extends AppCompatActivity {
+
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
+    FirebaseUser user;
+    StorageReference storageReference;
+    TextView fullName ;
 
     GridView gridView;
 
@@ -23,6 +42,39 @@ public class homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        fullName = findViewById(R.id.name);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        userId = fAuth.getCurrentUser().getUid();
+        user = fAuth.getCurrentUser();
+
+        if(user!=null) {
+            final DocumentReference documentReference = fStore.collection("users").document(userId);
+            documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+                @Override
+                public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                    fullName.setText("Hello "+documentSnapshot.getString("fName"));
+                }
+            });
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         gridView = findViewById(R.id.gridview);
